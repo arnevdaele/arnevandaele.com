3b0925960d2eda3c6a110ecc9e9e96f93af1bd5c
Arne Van Daele <arne.van.daele@icloud.com>
2023-11-23 17:45:23 +0100

    push img

--- arnevandaele/src/components/Header.js
+++ arnevandaele/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='flex w-screen text-center justify-center text-white bg-gradient-to-tr from-indigo-500 to-indigo-900 py-12 lg:py-24'>
           <div className='flex flex-col items-center'>
-            <img className='h-28 w-28 rounded-full border-2 border-white/10 drop-shadow-lg mb-5 lg:mb-8' src='https://www.arnevandaele.com/assets/arne.jpg'></img>
+            <img className='h-28 w-28 rounded-full border-2 border-white/10 drop-shadow-lg mb-5 lg:mb-8' src='arne.jpg'></img>
             <div className='font-bold text-2xl lg:text-4xl'>Arne Van Daele</div>
             <div className='text-leading font-light italic'>Full-stack developer</div>
           </div>
