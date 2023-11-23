e05fde63c77f07188e907c6183f1604cbef21611
Arne Van Daele <arne.van.daele@icloud.com>
2023-11-23 17:43:01 +0100

    init

---
+++ arnevandaele/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className='h-screen w-auto bg-gray-100'>
+      <Header />
+      
+      <div className='flex justify-center mt-8'>
+        Deze module is tijdelijk niet beschikbaar.
+      </div>
+    </div>
+  );
+}
+
+export default App;
